4b5fe2fa59b37ad5052d56f846053648159a5c94
Holnyx <sarakomaichi@gmail.com>
2024-05-30 14:54:53 +0300

    Updates

--- _next/static/chunks/app/pages/HomePage/page-ae847f65c7dfb743.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[782],{1011:function(n,e,u){Promise.resolve().then(u.bind(u,7047))}},function(n){n.O(0,[365,383,47,971,23,744],function(){return n(n.s=1011)}),_N_E=n.O()}]);
