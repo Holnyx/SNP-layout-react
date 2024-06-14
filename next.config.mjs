a58745604957b64b82b0ac09cefdc51438271119
Holnyx <sarakomaichi@gmail.com>
2024-06-14 14:22:48 +0300

    Rebuilding

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const nextConfig = {
   // output: 'export',
