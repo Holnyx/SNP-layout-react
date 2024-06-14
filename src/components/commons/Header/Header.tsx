7e9c903a2374f11368d360a307cf22ef62104a80
Holnyx <sarakomaichi@gmail.com>
2024-06-14 13:56:03 +0300

    Recreate the file headerLinkItems

--- src/components/commons/Header/Header.tsx
+++ src/components/commons/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-scroll";
-import  headerLinkState  from './headerLinkState'
 import  Icon  from '../Icon';
+import headerLinkState from './headerLinkItems';
 
 
 const Header = () => {
