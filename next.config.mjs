e7af5015b0c9f4185049ef6e136ad3de5b50ecd4
Holnyx <sarakomaichi@gmail.com>
2024-06-17 12:40:35 +0300

    Add setting alias

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,11 @@
 // import { fileURLToPath } from 'url';
 import path from 'path';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const nextConfig = {
-  // output: 'export',
-  // assetPrefix: '/SNP-layout-react/',
-  // basePath: '/SNP-layout-react',
-  // trailingSlash: true,
-
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: '**.holnyx.github.io',
-  //       port: '',
-  //     },
-  //   ],
-  // },
-
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
-
-  // webpack(config) {
-  //   // SVGR configuration
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['@svgr/webpack'],
-  //   });
-
-  //   return config;
-  // },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve('src');
+    return config;
+  },
 };
 
 export default nextConfig;
-
